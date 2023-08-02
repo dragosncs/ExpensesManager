9e397ee9bb2e5480417dc426bac3504475f24347
Dragos Neacsu <dragosncs@gmail.com>
2023-08-02 14:53:03 +0300

    Initial Commit

---
+++ IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by Dragos Neacsu on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
